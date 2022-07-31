b1c086d50c6f6ee18b2e92760d38ce69206addae
ErmiasMerine <ebm361@nyu.edu>
2022-07-31 16:30:20 -0400

    Fixed styling for product ratings and amazon prime logo, made grid of product pages look more authentic by adding an amazon banner and randomizing image size of product component, changed amount of products showed per column based on page size using tailwind

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,24 +16,24 @@ function Product({id, title, price, description, category, image}){
 
             <Image src={image} height={200} width={200} objectFit="contain" />
 
-            <h4>{title}</h4>
+            <h4 className="my-3">{title}</h4>
             <div className = "flex">
                 {Array(rating)
                     .fill()
                     .map((_, i) => (
-                        <StarIcon className="h-5"/>
+                        <StarIcon className="h-5 text-yellow-500"/>
                     ))}
             </div>
 
             <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-            <div>
+            <div className="mb-5">
                 <Currency quantity={price} />
             </div>
 
             {hasPrime && (
                 <div className="flex items-center space-x-2 -mt-5">
-                    <img src="https://links.papareact.com/fdw" alt="" />
+                    <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
                     <p className = "text-xs text-gray-500">FREE Next-day Delivery</p>
                 </div>
             )}
