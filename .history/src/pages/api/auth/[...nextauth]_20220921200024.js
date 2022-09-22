6bd22526b1344435d5f019c342d2b739e08dfa7b
ErmiasMerine <ebm361@nyu.edu>
2022-09-21 20:11:11 -0400

    Slight changes to env 3

---
+++ .history/src/pages/api/auth/[...nextauth]_20220921200024.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  SECRET: process.env.NEXTAUTH_SECRET,
+
+})
+
+//id 418165183030-s32c9btlfss38m0uk9gn0r62njk45v98.apps.googleusercontent.com
+//secret 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
